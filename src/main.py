7e9269f6a73b16a8ee17f28034cae0a66de656af
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-12-26 20:54:34 +0100

    hmm

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextNode
+
+
+def main():
+    fisk = "This is some anchor text, link, https://www.boot.dev"
+    node = TextNode(fisk)
+    print(node)
+
+
+main()
