084a0527024a579e8ade7ca656cb4b8c34231acd
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-12-26 23:35:14 +0100

    småting

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,13 +9,13 @@ class HTMLNode:
         raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
-        if self.props == None:
+        if self.props is None:
             return ""
         the_string = ""
         for prop in self.props:
             the_key = self.props[prop]
-            the_string += f'{prop}="{the_key}" '
+            the_string += f' {prop}="{the_key}"'
         return the_string
 
     def __repr__(self):
-        return f"HTMLNode: tag={self.tag} value={self.value} children={self.children} props={self.props}"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,17 +4,52 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_html_node(self):
+    def test_to_html_props(self):
         node = HTMLNode(
-            "h1",
-            "the text inside a paragraph",
-            ["h1, p"],
-            {
-                "href": "https://www.google.com",
-                "target": "_blank",
-            },
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
         )
         self.assertEqual(
-            "HTMLNode: tag=h1 value=the text inside a paragraph children=['h1, p'] props={'href': 'https://www.google.com', 'target': '_blank'}",
-            repr(node),
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
         )
+
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
