6956bd28e31d85785499e59659c3e96bb339ae9a
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-12-27 18:30:52 +0100

    test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -34,3 +34,16 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("No tag")
+        if self.children is None:
+            raise ValueError("No children")
+
+    retu
