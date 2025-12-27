cb4b2447aca985e3a7e59bf6ebd40034c166be48
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-12-27 17:12:32 +0100

    små rettelser

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,19 +22,15 @@ class HTMLNode:
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, children=None, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.value == None:
-            raise ValueError("All leaf nodes must have a value.")
-
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
         if self.tag is None:
             return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
-        props_html = ""
-        if self.props:
-            for key, val in self.props.items():
-                props_html += f' {key}="{val}"'
-
-        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
