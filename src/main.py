4fccfa59ad75aaf5bcbabf9e154ae1104c2cfc4c
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-12-27 22:18:27 +0100

    ændre main.py tilbage

--- src/main.py
+++ src/main.py
@@ -1,15 +1,9 @@
-from split_delimiter import split_nodes_delimiter
 from textnode import TextNode, TextType
 
 
 def main():
-    # node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    # print(node)
-    fisk = "This is text with a **bolded phrase** in the middle"
-    # split_nodes_delimiter()
-
-    node = TextNode("This is text with a `code block` word", TextType.TEXT)
-    split_nodes_delimiter([node], "`", TextType.CODE)
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
 
 
 main()
