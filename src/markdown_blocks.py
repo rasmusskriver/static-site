ca426c84015f094839a352b731374fd4b18de47d
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-12-29 20:12:52 +0100

    tilføfje så den kan håndtere mange mellemrum i block function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -2,5 +2,7 @@ def markdown_to_blocks(markdown):
     lines = markdown.split("\n\n")
     new_list = []
     for line in lines:
+        if line == "":
+            continue
         new_list.append(line.strip())
     return new_list
