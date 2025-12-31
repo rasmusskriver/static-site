06c18ba311de62cc6bbe16d1222e85c5b131fb85
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-12-31 15:10:21 +0100

    igang med Block Types

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
+from markdown_blocks import markdown_to_blocks
 from textnode import TextNode, TextType
 
-
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    # node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    # print(node)
+
+    md="""
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+    print(markdown_to_blocks(md))
 
 
 main()
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,19 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(markdown_block):
+    pass
+
+
 def markdown_to_blocks(markdown):
     lines = markdown.split("\n\n")
     new_list = []
