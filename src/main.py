d8c08bde6600893b925355106dea32369a00ba0c
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2026-01-01 18:59:50 +0100

    igang med block to html

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from markdown_blocks import markdown_to_blocks
+from markdown_to_html import markdown_to_html_node
 from textnode import TextNode, TextType
+
 
 def main():
     # node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     # print(node)
 
-    md="""
+    md = """
 This is **bolded** paragraph
 
 This is another paragraph with _italic_ text and `code` here
@@ -14,7 +16,7 @@ This is the same paragraph on a new line
 - This is a list
 - with items
 """
-    print(markdown_to_blocks(md))
+    print(markdown_to_html_node(md))
 
 
 main()
